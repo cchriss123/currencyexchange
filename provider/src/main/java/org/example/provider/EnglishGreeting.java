c0b81710c724571922ea1e2aa65afcb52c96168f
Christofer Nyström <christofer.nystrom@iths.se>
2023-04-19 21:36:46 +0200

    Adds provider

---
+++ provider/src/main/java/org/example/provider/EnglishGreeting.java
@@ -0,0 +1,11 @@
+package org.example.provider;
+
+import org.example.service.Greeting;
+
+public class EnglishGreeting implements Greeting {
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+
+}
