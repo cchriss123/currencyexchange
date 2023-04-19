e5a816e0995e78cfd8ce3f0082aa215c620365a6
Christofer Nyström <christofer.nystrom@iths.se>
2023-04-19 23:08:42 +0200

    Adds module info

--- consumer/src/main/java/org/example/consumer/Consumer.java
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -2,15 +2,21 @@ package org.example.consumer;
 
 import org.example.service.Greeting;
 
+import java.util.ServiceLoader;
+
+
+
 public class Consumer {
 
     public static void main(String[] args) {
 
+        ServiceLoader<Greeting> loader = ServiceLoader.load(Greeting.class);
+        for (Greeting greeting : loader) {
+            System.out.println(greeting.sayHello());
+        }
 
-        Greeting greeting;
 
 
-      //  System.out.println(greeting.sayHello());
 
 
     }
