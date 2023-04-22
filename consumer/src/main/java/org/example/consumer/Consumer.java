502fc283b24ba775fff862b604ceb377b22a08dc
Christofer Nyström <christofer.nystrom@iths.se>
2023-04-22 19:41:44 +0200

    Adds exchange service

--- consumer/src/main/java/org/example/consumer/Consumer.java
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -3,16 +3,25 @@ package org.example.consumer;
 import org.example.service.Greeting;
 import org.example.service.annotation.Language;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ServiceLoader;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public class Consumer {
 
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, IOException, NoSuchMethodException, InstantiationException {
 
-        ServiceLoader<Greeting> loader = ServiceLoader.load(Greeting.class);
+//        ServiceLoader<Greeting> loader = ServiceLoader.load(Greeting.class);
+//        for (var greeting : loader) {
+//            System.out.println(greeting.sayHello());
+//        }
+
 
         Set<Class> classes = findAllClasses("org.example.provider");
 
@@ -20,30 +29,38 @@ public class Consumer {
            var annotation = (Language) c.getAnnotation(Language.class);
            if (annotation!=null){
                System.out.println(annotation.value());
-               var methods = c.getMethods();
-               for(var m :methods){
-                   if(m.getParameterTypes().equals(String.class) && m.getParameterCount() == 0) {
-                     //  var s = (String) m.invoke(c);
-                       var s = m.invoke(c);
+               var o = c.getConstructor().newInstance();
 
-                       if (s instanceof String string)
-                           System.out.println(string);
+               var methods = c.getMethods();
+               for (var m :methods){
+                   if (m.getParameterTypes().equals(String.class) && m.getParameterCount() == 0 && !m.getName().equals("toString")){
+                       var s = (String) m.invoke(o);
+                       System.out.println(s);
                    }
                }
-
            }
         }
-
-
-
-
-        for (var greeting : loader) {
-            System.out.println(greeting.sayHello());
-        }
-
     }
 
-    private static Set<Class> findAllClasses(String s) {
-        return Set.of();
+    public static Set<Class> findAllClasses(String packageName) {
+
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+        return reader.lines()
+                .filter(line -> line.endsWith(".class"))
+                .map(line -> getClass(line, packageName))
+                .collect(Collectors.toSet());
+    }
+
+    private static Class getClass(String className, String packageName) {
+        try {
+            return Class.forName(packageName + "." + className.substring(0, className.lastIndexOf('.')));
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- provider/src/main/java/module-info.java
+++ provider/src/main/java/module-info.java
@@ -1,3 +1,4 @@
+import org.example.provider.AnnotatedGreeting;
 import org.example.provider.EnglishGreeting;
 import org.example.provider.SpanishGreeting;
 import org.example.provider.SwedishGreeting;
@@ -6,6 +7,7 @@ import org.example.service.Greeting;
 
 module org.example.provider {
 
+    exports org.example.provider;
     requires org.example.service;
 
     provides Greeting with SwedishGreeting, EnglishGreeting, SpanishGreeting;
