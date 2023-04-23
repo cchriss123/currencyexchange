82352e8d10dcc110676754f8df8e3fcd916f62d8
Christofer Nyström <christofer.nystrom@iths.se>
2023-04-23 23:52:11 +0200

    small refactor main

--- consumer/src/main/java/org/example/consumer/Consumer.java
+++ consumer/src/main/java/org/example/consumer/Consumer.java
@@ -6,38 +6,35 @@ import org.example.service.annotation.CurrencyCode;
 import java.io.IOException;
 import java.util.*;
 
-
 public class Consumer {
 
     public static void main(String[] args) throws IOException{
 
-
         Scanner scanner = new Scanner(System.in);
         String fromCurrency = "SEK";
-        Map<String, CurrencyExchangeService> currencyServices = new HashMap<>();
+        Map<String, CurrencyExchangeService> currencyPrices = new HashMap<>();
         ServiceLoader<CurrencyExchangeService> loader = ServiceLoader.load(CurrencyExchangeService.class);
 
 
         for (CurrencyExchangeService service : loader) {
             CurrencyCode annotation = service.getClass().getAnnotation(CurrencyCode.class);
             if (annotation != null) {
-                currencyServices.put(annotation.value(), service);
+                currencyPrices.put(annotation.value(), service);
             }
         }
 
         System.out.println("Enter the currency code to see currency prices from SEK (USD or EUR):");
-
         String toCurrency = "";
-        while (!currencyServices.containsKey(toCurrency)) {
+        while (!currencyPrices.containsKey(toCurrency)) {
             toCurrency = scanner.nextLine().trim().toUpperCase();
-            if (!currencyServices.containsKey(toCurrency)) {
+            if (!currencyPrices.containsKey(toCurrency)) {
                 System.out.println("Invalid input. Please enter a valid currency code (USD or EUR).");
             }
         }
 
-        CurrencyExchangeService exchangeService = currencyServices.get(toCurrency);
-        double exchangeRate = exchangeService.getExchangeRate(fromCurrency, toCurrency);
-        System.out.println("Exchange rate from " + fromCurrency + " to " + toCurrency + ": " + String.format("%.4f", exchangeRate));
+        double currencyPrice = currencyPrices.get(toCurrency).getExchangeRate(fromCurrency, toCurrency);
+
+        System.out.println("Price from " + fromCurrency + " to " + toCurrency + ": " + String.format("%.3f", currencyPrice));
 
 
     }
